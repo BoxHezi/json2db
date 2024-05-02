0aefcbf48d44801fa94b94bf65ad1a4c9b5c77de
boxhezi <547181293@qq.com>
2024-05-02 11:32:43 +1000

    add support for other input json file format; add -k for upsert operation

--- json2mongo.py
+++ json2mongo.py
@@ -12,6 +12,7 @@ def init_argparse():
     args.add_argument("-u", "--user", help="MongoDB username")
     args.add_argument("-P", "--password", help="MongoDB password")
 
+    args.add_argument("-k", "--key", help="Specificing key to check for upsert operation\nIf not specified, all records will be inserted")
     args.add_argument("-d", "--database", help="Specificing database name", required=True)
     args.add_argument("-c", "--collection", help="Specificing collection name", required=True)
     args.add_argument("-f", "--file", help="JSON file path", required=True)
@@ -26,22 +27,23 @@ def construct_mongo_uri(address, port, user: str = None, password: str = None):
 
 
 
-def main(mongo_str: str, file_path: str, database: str, collection: str):
+def main(mongo_str: str, file_path: str, database: str, collection: str, key: str):
     client = None
     try:
         client = pymongo.MongoClient(mongo_str)
     except Exception as e:
         print(e)
 
-    data_list = []
-    with open(file_path, 'r') as f:
-        data_list = [json.loads(line.strip()) for line in f]
+    data_list = read_file_content(file_path)
     db = client[database]
     table = db[collection]
 
     try:
         for data in data_list:
-            table.replace_one({"host": data["host"]}, data, upsert=True)
+            if key:
+                table.replace_one({key: data[key]}, data, upsert=True)
+            else:
+                table.insert_one(data)
     except pymongo.errors.ServerSelectionTimeoutError as e:
         print(f"Please check your MongoDB connection\n{e}")
     except pymongo.errors.OperationFailure as e:
@@ -51,8 +53,34 @@ def main(mongo_str: str, file_path: str, database: str, collection: str):
         client.close()
 
 
+def read_file_content(file_path: str) -> list[dict]:
+    contents = []
+
+    try:
+        f = open(file_path, "r")
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+
+    try:
+        contents = json.load(f)  # when input file is: [{}, {}, {}]
+    except json.decoder.JSONDecodeError:  # when input file is not a regular JSON, it contains a JSON obj each line instead
+        # when input file is: {}\n{}\n{}\n...\n{}
+        f.close()
+        f = open(file_path, "r")
+        contents = [json.loads(line.strip()) for line in f]
+
+    if f:
+        f.close()
+
+    return contents
+
+
 if __name__ == "__main__":
     args = init_argparse()
     mongo_str = construct_mongo_uri(args.address, args.port, args.user, args.password)
-    main(mongo_str, args.file, args.database, args.collection)
+    main(mongo_str, args.file, args.database, args.collection, args.key)
+
+    # DEBUG for different type JSON contents
+    # read_file_content("./1_vulns_ips_http_non_cdn_asn_10148.json")
+    # read_file_content("./dnsx_unimelb.edu.au.json")
 
